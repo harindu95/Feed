e8738e16e54d963f57f01c5361f1b20dff7a44ce
Harindu Dilshan ( Kavinda Pitiduwa Gamage) <harindudilshan95@gmail.com>
2023-10-31 21:17:16 -0600

    urls stored in a seperate file

---
+++ config.py
@@ -0,0 +1,6 @@
+urls = [
+        'https://www.theverge.com/rss/frontpage',
+        'https://lifehacker.com/rss',
+        'http://www.reddit.com/r/Buddhism/.rss',
+        'https://www.youtube.com/feeds/videos.xml?playlist_id=PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-',
+    ]
