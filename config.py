0aedf8079a574591a0432f6b2c1dc5e40bc33508
Harindu Dilshan ( Kavinda Pitiduwa Gamage) <harindudilshan95@gmail.com>
2023-11-03 16:23:46 -0600

    more rss feeds

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 urls = [
-    'https://www.theverge.com/rss/frontpage',
-    'https://lifehacker.com/rss',
-    'http://www.reddit.com/r/Buddhism/.rss',
-    'https://www.youtube.com/feeds/videos.xml?playlist_id=PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-',
+        'https://www.theverge.com/rss/frontpage',
+        'https://lifehacker.com/rss',
+        'http://www.reddit.com/r/Buddhism/.rss',
+        'https://www.youtube.com/feeds/videos.xml?playlist_id=PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-',
+        'https://www.omgubuntu.co.uk/feed',
+        'https://tinybuddha.com/feed',
+        'https://time.com/feed',
+        'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',
+        'https://news.ycombinator.com/rss'
 ]
