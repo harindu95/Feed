9246ec56aef764dd1220db2059132c69ec640208
Harindu Dilshan ( Kavinda Pitiduwa Gamage) <harindudilshan95@gmail.com>
2023-11-05 17:52:24 -0700

    Delegate for Card class

--- card.py
+++ card.py
@@ -1,7 +1,10 @@
 from PyQt6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QFrame
-from PyQt6.QtGui import QTextDocument, QPixmap
-from PyQt6.QtCore import Qt, QUrl
+from PyQt6.QtCore import Qt
 from PyQt6.QtCore import pyqtSignal, QObject
+from PyQt6.QtGui import QPainter, QPixmap,QRegion
+from PyQt6.QtWidgets import QStyle,QStyleOptionViewItem,QWidget, QVBoxLayout, QListView, QStyledItemDelegate, QLabel
+from PyQt6.QtCore import QModelIndex, QSize, QRect
+
 import sys
 import requests
 
@@ -25,53 +28,95 @@ class Connection(QObject):
 
 class Card(QFrame):
 
-    def __init__(self, entry, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.entry = entry
-        self.c = Connection()
+    def setItem(self, item):
+        self.clear()
+        self.item = item
+        if self.item.image_url != '':
+            image = self.item.image_data
+            pixmap = QPixmap()
+            pixmap.loadFromData(image)
+            self.icon.show()
+            pixmap = pixmap.scaled(
+                200, 160, Qt.AspectRatioMode.KeepAspectRatio,transformMode = Qt.TransformationMode.SmoothTransformation)
+            self.icon.setPixmap(pixmap)
+        else:
+            print("No Image URL found!")
 
-        icon = QLabel()
+        self.title.setText(process_description(item.title))
+        content = process_description(item.summary)
+        self.subtitle.setText(trim_description(content))
+
+    def clear(self):
+        self.icon.setPixmap(QPixmap())
+        self.icon.hide()
+        self.title.setText('')
+        self.subtitle.setText('')
+   
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.icon = QLabel()
         description = QWidget()
         layout = QHBoxLayout()
         self.setLayout(layout)
-
-        if self.entry.image_url != '':
-            image = self.entry.image_data
-            self.pixmap = QPixmap()
-            self.pixmap.loadFromData(image)
-            self.pixmap = self.pixmap.scaled(
-                200, 160, Qt.AspectRatioMode.KeepAspectRatio,transformMode = Qt.TransformationMode.SmoothTransformation)
-            icon.setPixmap(self.pixmap)
-            icon.setFixedWidth(160)
-            layout.addWidget(icon)
-
-        else:
-            print("No Image URL found!")
+        self.icon.setFixedWidth(160)
+        layout.addWidget(self.icon)        
 
         layout.addWidget(description)
         title_layout = QVBoxLayout()
 
         self.setObjectName("card")
-        # self.setStyleSheet("border: 1px solid red;")
-        # self.setStyleSheet(
-        #     "QFrame#Card{ border-top:3px solid gray; background:white; }")
 
-        title = QLabel()
-        title.setText(process_description(entry.title))
-        title.setObjectName('title')
-        # title_font = title.font()
-        # title_font.setPointSize(12)
-        # title.setFont(title_font)
-        title.setWordWrap(True)
-        title_layout.addWidget(title)
+        self.title = QLabel()        
+        self.title.setObjectName('title')
+        self.title.setWordWrap(True)
+        font = self.title.font()
+        font.setPointSize(14)
+        self.title.setFont(font)
+        title_layout.addWidget(self.title)
         description.setLayout(title_layout)
 
-        self.content = process_description(entry.summary)
         self.hidden = True
-        subtitle = QLabel()
-        subtitle.setText(trim_description(self.content))
-        subtitle.setObjectName('subtitle')
-        subtitle.setWordWrap(True)
-        title_layout.addWidget(subtitle)
+        self.subtitle = QLabel()
+        self.subtitle.setObjectName('subtitle')
+        self.subtitle.setWordWrap(True)
+        title_layout.addWidget(self.subtitle)
+        # self.mousePressEvent = lambda event: self.c.clicked.emit()
+        self.get_stylesheet()
 
-        self.mousePressEvent = lambda event: self.c.clicked.emit()
+    def get_stylesheet(self,file='card.css'):
+        css = '\n'.join(open(file).readlines())
+        self.setStyleSheet(css)
+
+
+class CardDelegate(QStyledItemDelegate):
+
+    def __init__(self ) -> None:
+        super().__init__()
+        self.card = Card()
+
+    def sizeHint(self, option: QStyleOptionViewItem, index: QModelIndex) -> QSize:
+        item = index.data()
+        self.card.setItem(item)
+        rect = QRect(0,0,450,200)
+        self.card.setGeometry(rect)
+        width = 450
+        # self.card.setFixedWidth(width)
+        height = self.card.size().height()
+        size = QSize()
+        size.setHeight(height)
+        size.setWidth(width)
+        return size
+
+    def paint(self, painter: QPainter , option: QStyleOptionViewItem, index: QModelIndex) -> None:
+        painter.save()
+        item = index.data()
+        self.card.setItem(item)
+        painter.translate(option.rect.x(),option.rect.y())
+        self.card.setGeometry(option.rect)
+        region = QRegion(0,0, option.rect.width(),option.rect.height())
+        p = QPoint()
+        p.setX(0)
+        p.setY(0)
+        self.card.render(painter,targetOffset=p,sourceRegion=region)
+        painter.restore()
---
+++ item-delegate.py
@@ -0,0 +1,117 @@
+from PyQt6.QtGui import QPainter, QPixmap,QFont, QTextDocument, QAbstractTextDocumentLayout, QColor,QRegion
+from PyQt6.QtWidgets import QStyle,QStyleOptionViewItem,QStyledItemDelegate
+from PyQt6.QtCore import QModelIndex, QSize, QRect, QRectF, Qt
+
+
+class ItemDelegate(QStyledItemDelegate):
+
+    def get_image_size(self):
+        image_width,image_height = 200,160
+        return image_width,image_height
+
+    def __init__( self ):
+            super().__init__()
+            # probably better not to create new QTextDocuments every ms
+            self.doc = QTextDocument()
+
+    def process_description(self, txt):
+        from bs4 import BeautifulSoup
+
+        plain = ' '.join(BeautifulSoup(txt, "html.parser").findAll(text=True))
+        return plain.strip()[:200]
+    
+    def sizeHint(self, options, index: QModelIndex) -> QSize:
+        # options = QtGui.QStyleOptionViewItemV4(option)
+        # self.initStyleOption(options,index)
+        image_width,image_height = self.get_image_size()
+        padding = 20
+        item = index.data()
+        if item.image_url == '':
+            image_width,image_height = 0,0
+
+        width = options.rect.width() - image_width
+        self.doc.setTextWidth(width)
+        font = QFont(options.font)
+        font.setPointSize(14)
+        self.doc.setDefaultFont(font)
+        self.doc.setHtml(item.title)
+        title_height = self.doc.size().height()
+
+        summary = self.process_description(item.summary)
+        self.doc.setDefaultFont(options.font)
+        self.doc.setHtml(summary)
+        summary_height = self.doc.size().height()
+        
+        size = QSize()
+        size.setWidth(options.rect.width())
+        h2 = padding + title_height + padding + summary_height
+        h = max(image_height + padding + padding , h2 ) 
+        # h = max(height, image_height + padding * 2 )
+        h = h + 2 #line height
+        size.setHeight(h)
+        return size
+
+    def paint(self, painter: QPainter, options:QStyleOptionViewItem, index: QModelIndex) -> None:
+        painter.save()
+        # options.widget.style().drawControl(QStyle.ControlElement.CE_ItemViewItem, options, painter)
+        painter.translate(options.rect.left(), options.rect.top())
+
+        item = index.data()
+        x = options.rect.x()
+        y = options.rect.y()
+        width = options.rect.width()
+        height = options.rect.height()
+        padding = 20
+        painter.translate(0, padding)
+        height -= padding
+        image_width,image_height = self.get_image_size()
+
+        if item.image_url != '':
+            image = item.image_data
+            pixmap = QPixmap()
+            pixmap.loadFromData(image)
+            pixmap = pixmap.scaled(
+                image_width, image_height, Qt.AspectRatioMode.KeepAspectRatio,transformMode = Qt.TransformationMode.SmoothTransformation)
+            painter.drawPixmap(QRect(0,0,image_width,image_height) ,pixmap)
+            
+        else:
+            image_width,image_height = 0,0
+
+        width -= padding
+        painter.translate(image_width + padding, 0)
+        width -= image_width
+
+        self.doc.setHtml(item.title)
+        font = QFont(options.font)
+        font.setPointSize(14)
+        self.doc.setDefaultFont(font)
+        clip = QRectF(0, 0, width, height)
+        ctx = QAbstractTextDocumentLayout.PaintContext()
+        ctx.clip = clip
+        self.doc.setTextWidth(width)  
+        self.doc.documentLayout().draw(painter, ctx)
+        title_height = self.doc.size().height()
+        
+        painter.translate(0, title_height )
+        height = height - title_height
+
+        summary = self.process_description(item.summary)
+        self.doc.setHtml(summary)
+        font = options.font
+        self.doc.setDefaultFont(options.font)
+        clip = QRectF(0,0,width, height)
+        ctx = QtGui.QAbstractTextDocumentLayout.PaintContext()
+        ctx.clip = clip
+        self.doc.documentLayout().draw(painter, ctx)
+        summary_height = self.doc.size().height()
+        painter.translate(0,summary_height)
+
+        h2 = title_height + summary_height
+        if h2 > image_height:
+            painter.translate(-image_width -padding, 0)
+            painter.fillRect(0,0,options.rect.width(),2, QColor(130,120,130))
+        else:
+            painter.translate(-image_width -padding, image_height-h2)
+            painter.fillRect(0,0,options.rect.width(),2, QColor(130,120,130))
+        painter.restore()
+
