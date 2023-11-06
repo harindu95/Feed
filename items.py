755a8b52197d6039b8e52ad84d293f7bed2fbd3b
Harindu Dilshan ( Kavinda Pitiduwa Gamage) <harindudilshan95@gmail.com>
2023-11-05 17:54:09 -0700

    Organize and sort items

--- items.py
+++ items.py
@@ -1,33 +1,106 @@
+import typing
+from PyQt6 import QtCore, QtGui
 import requests
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QTextEdit, QFrame
-from PyQt6.QtGui import QTextDocument, QPixmap
-from PyQt6.QtCore import QUrl, Qt
+from PyQt6.QtWidgets import QStyle,QStyleOptionViewItem,QWidget, QVBoxLayout, QListView, QStyledItemDelegate, QLabel
+from PyQt6.QtCore import QModelIndex, QObject, QUrl, Qt, QTimer, QThread, QAbstractListModel, QVariant, QRect, QSize, QPoint
 from card import Card
-from queue import Queue, Empty
+from common import GenericWorker, AsyncGeneratorWorker
+
+
+class ItemsModel(QAbstractListModel):
+
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.list = []
+
+    def rowCount(self, parent: QModelIndex = QModelIndex()) -> int:
+        return len(self.list)
+
+    def data(self, index: QModelIndex, extra):
+        if index.row() < len(self.list):
+            return self.list[index.row()]
+        else:
+            return QVariant()
+
+    def setData(self, index: QModelIndex, value) -> bool:
+        i = index.row()
+        if i < len(self.list):
+            self.list[i] = value
+        elif i == len(self.list):
+            self.list.insert(i,value)
+        else:
+            return False
+        
+        self.dataChanged.emit(index, index)
+        return True
+        
+    def addItem(self, item):
+        index = self.createIndex(len(self.list), 1,item)
+        self.setData(index, item)
 
 
 class Items(QWidget):
 
-    def __init__(self, view, scroll, settings, *args, **kwargs):
+    def __init__(self, view, scroll, application, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.scroll = scroll
-        self.settings = settings
-        self.items = []
+        self.application = application
+        # self.items = []
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.view = view
-        self.updateUi()
+        self.background_thread = application.createThread()
+        self.background_thread.start()
+        self.organize_running = False
+        self.list_view = QListView()
+        self.layout.addWidget(self.list_view)
+        self.list_model = ItemsModel()
+        self.list_view.setModel(self.list_model)
+        self.list_view.clicked.connect(self.clickItem)
+        from card import CardDelegate
+        delegate = CardDelegate()
+        # delegate = ItemDelegate()
+        self.list_view.setItemDelegate(delegate)
+        self.list_view.setLayoutMode(QListView.LayoutMode.Batched)
+        self.setObjectName('items')
+        # QTimer.singleShot(0,self.fetch_feeds)
+        self.fetch_feeds()
 
+    def clickItem(self, index):
+        item = index.data()
+        self.view.page().load(QUrl(item.link))
+
+    def remove_bottom_item(self):
+        try:
+            item = self.view_items.pop()
+            self.remove_item(item)
+        except Exception:
+            return
+        
     def add_item(self, item):
-        self.items.append(item)
         card = Card(item)
-        card.c.clicked.connect(lambda item=item: self.clickItem(item))
+        self.view_items.append(item)
+        item.cardIndex = len(self.cards)
+        item.index = len(self.cards)
+        self.cards.append(card)
+        # item.widget = card
+        item.rank = 0
+
+        card.c.clicked.connect(lambda card=card: self.clickItem(card))
         self.layout.addWidget(card)
-        self.updateGeometry()
 
     def add_items(self, data):
-        for entry in data.entries:
-            self.add_item(entry)
+        if data is None:
+            return
+        for feed in data:
+            for item in feed.entries:
+                self.list_model.addItem(item)
+
+
+    def fetch_feeds(self):
+
+        from feeds import updateFeeds
+        updateFeeds(self.application, self.add_items)
 
     def remove_all_items(self):
         for i in range(self.layout.count()):
@@ -36,20 +109,57 @@ class Items(QWidget):
             if widget is not None:
                 widget.setParent(None)
 
-    def updateUi(self):
-        self.remove_all_items()
-        # self.items.reverse()
-        for item in self.items:
-            card = Card(item)
-            card.c.clicked.connect(lambda item=item: self.clickItem(item))
-            self.layout.addWidget(card)
-        self.updateGeometry()
+    def sort_items(self, items):
+        if items == self.items:
+            return
+        else:
+            self.items = items
+        for i, item in enumerate(self.items):
+            if 'cardIndex' in item and item.cardIndex >= 0:
+                card = self.cards[item.cardIndex]
+                self.layout.removeWidget(card)
+                # print(i,item.title,item.rank)
+                self.layout.insertWidget(i, card)
+                item.cardIndex = i
+                # item.index = i
+
+        self.scroll.verticalScrollBar().setValue(0)
+
 
     def initialize(self, feeds):
         self.add_items(feeds)
 
-    def clickItem(self, entry):
-        self.view.load(QUrl(entry.link))
+
     def set_wordclouds(self, wordclouds):
         print('wordclouds', len(wordclouds))
-        self.settings.word_clouds = wordclouds
+        self.application.word_clouds = wordclouds
+
+    from typing import List
+
+    def organize_items(items: List[dict], corpus):
+        from keywords import get_centroids, rank_items, generateWordClouds
+        centroids = get_centroids(corpus)
+        word_clouds = generateWordClouds(centroids)
+        items = rank_items(items, centroids)
+        items.sort(reverse=True, key=lambda x: x.rank)
+        return (items, word_clouds)
+
+    def handleOrganize(self, result):
+        self.organize_running = False
+        items, word_clouds = result
+        self.set_wordclouds(word_clouds)
+        self.sort_items(items)
+
+    def organize(self):
+        # from keywords import corpus
+        if self.organize_running:
+            print("Organize already running!")
+            return
+        self.organize_running = True
+        items = self.items[:]
+        corpus = self.application.corpus
+        sortWorker = GenericWorker(Items.organize_items, items, corpus)
+        sortWorker.moveToThread(self.background_thread)
+        sortWorker.finished.connect(
+            self.handleOrganize, Qt.ConnectionType.QueuedConnection)
+        sortWorker.start.emit()
